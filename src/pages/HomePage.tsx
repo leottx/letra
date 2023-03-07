68d2e1099bf96454b94d002ee70c05498affc542
leotx <tteixeira.leonardo@gmail.com>
2023-03-07 17:06:33 -0300

    chore: instala o react-router-dom e outra dependencias, alem de adicionar novas configuracoes no tailwind e tsconfig

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,11 @@
+export const HomePage = () => {
+  return (
+    <div className="w-full h-screen bg-neutral-800">
+      <div className="flex flex-col justify-between p-4">
+        <h1 className="text-4xl text-neutral-100 font-black text-center">
+          Termo Clone
+        </h1>
+      </div>
+    </div>
+  );
+};
