d2e40e7ca860ddf2c36bbc06b6f9a8ac8f711593
leotx <tteixeira.leonardo@gmail.com>
2023-03-08 10:01:35 -0300

    chore: instala novas dependencias e altera configuracoes do tailwind

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,11 +2,14 @@ const { fontFamily } = require("tailwindcss/defaultTheme");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       fontFamily: {
         sans: ["Montserrat", ...fontFamily.sans],
+      },
+      gridTemplateColumns: {
+        keysRow: "repeat(32, 1fr)",
       },
     },
   },
